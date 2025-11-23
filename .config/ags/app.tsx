b68f07d5f2a96a6a6468d54494480f3b54d37b60
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-23 13:29:41 -0300

    feat(ags): update app.tsx for gtk4 migration

--- .config/ags/app.tsx
+++ .config/ags/app.tsx
@@ -1,11 +1,17 @@
 import app from "ags/gtk4/app"
-import NotificationPopups from "./widget/Notification/NotificationPopups"
-import style from "./widget/Notification/style.scss"
+import style from "./style.scss"
+import Bar from "./widgets/Bar/Bar"
+import NotificationPopups from "./widgets/Notification/NotificationPopups"
 
 app.start({
   css: style,
   gtkTheme: "adw-gtk3-dark",
   main() {
+    // Create bar on each monitor
+    app.get_monitors().map(Bar)
+    
+    // Create notification popups
     NotificationPopups()
   },
 })
+
