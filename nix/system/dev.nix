24f42cb349646f5295950991bf908cd67eddb32e
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-23 17:50:51 -0300

    refactor: separated dev environments

--- nix/system/dev.nix
+++ nix/system/dev.nix
@@ -1,29 +1,15 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    # Build Tools & Compilers
-    gcc
-    clang
-    cmake
-    cargo
-
-    # Programming Languages & Runtimes
-    nodejs
-    bun
-    python3
-    go
-    gomodifytags
-    lua
-    luarocks
-    zig
-    dart
-    dotnet-sdk_9
-
-    # Language Servers & Formatters
-    lua-language-server
-    luaPackages.luacheck
-    stylua
-    # haskellPackages.hindent
+  imports = [
+    ./dev/c.nix
+    ./dev/dotnet.nix
+    ./dev/flutter.nix
+    ./dev/go.nix
+    ./dev/javascript.nix
+    ./dev/lua.nix
+    ./dev/python.nix
+    ./dev/rust.nix
+    ./dev/zig.nix
   ];
 }
